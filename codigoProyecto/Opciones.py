48d7eca728eee7d5bddd37cb6db96f2923dcab8e
ssh <sebas.herreraj23@gmail.com>
2022-10-14 00:20:46 -0600

    Intento de registro escuela

--- codigoProyecto/Opciones.py
+++ codigoProyecto/Opciones.py
@@ -7,24 +7,6 @@ h = 600
 fgcolor = '#FFEFD3'
 azul = '#001b2e'
 fuente = 'Lucida Sans'
-'''
-class mainform:
-    def __init__(self, master):
-        self.master = master
-        self.master.overrideredirect(1)
-        ws = self.master.winfo_screenwidth()
-        hs = self.master.winfo_screenheight()
-        x = (ws-w)/2
-        y = (hs-h)/2
-        self.master.geometry("%dx%d+%d+%d" % (w, h, x, y))
-
-        selflbl = tk.Label(self.master, text='Hola', font=('verdana',25, 'bold'),
-                            fg='#2A2C2B',bg="#ecf0f1")
-        selflbl.place(x=300,y=200)
-
-       
-        headerFrame = tk.Frame(self.master, bg='#001b2e', width=w, height=70)
-        headerFrame.pack()'''
 
 class mainform:
     def __init__(self, master):
@@ -53,7 +35,23 @@ class mainform:
         botonRE.place(x=60, y=130, width=300)
 
 
-        
+    
+    def registroEscuela(self, master):
+        registroFrame = tk.Frame(self.master, width=w, height=h)
+        registroFrame.pack()
+        registroEscLabel = tk.Label(registroFrame, text='Registro de escuela o area academica:', font=(fuente,16), fg='#2A2C2B',bg="#ecf0f1")
+        registroEscLabel.place(x=72, y=-70)
+
+        escuelaLabel = tk.Label(registroFrame, text='Nombre:', font=(fuente,16), fg='#2A2C2B',bg="#ecf0f1")
+        registroEscLabel.place(x=82, y=-20)
+        escuelaEntry = tk.Entry(registroFrame, font=(fuente,16))
+        escuelaEntry.place(x=120, y=-20, width=250)
+
+        botonRE = tk.Button(self.master, text='Registrar Escuela', borderwidth=1, relief='raised')
+        botonRE.config(bg=azul, font='Cambria 16 bold', fg= fgcolor)
+        botonRE.pack()
+        botonRE.place(x=60, y=130, width=300)
+        botonRE["command"] = registroEscuela()
 
 
 
